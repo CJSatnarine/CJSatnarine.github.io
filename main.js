5868249c57c366b0e29b7452641a44320dd7ef98
CJSatnarine <satnarinecj101@gmail.com>
2025-02-27 12:17:39 -0500

    js stuff

---
+++ main.js
@@ -0,0 +1,28 @@
+import * as THREE from 'three';
+import { OBJLoader } from 'three/addons/loaders/OBJLoader.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+document.body.appendChild(renderer.domElement);
+
+// Create Cube.
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x962FFE });
+const cube = new THREE.Mesh(geometry, material);
+
+// Add cube. 
+scene.add(cube);
+camera.position.z = 5;
+
+function animate() {
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+
+} 
